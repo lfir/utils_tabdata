6752c9246159215fba5eb48f621b5055e29c9639
Asta86 <psljp@protonmail.com>
2021-01-21 16:17:25 -0300

    Use compatible release specifier for the version of dependencies

--- setup.py
+++ setup.py
@@ -6,22 +6,29 @@ def readme():
     with open(file_name, 'r') as f:
         return f.read()
 
-
 setup(name='utils_tabdata',
-      version='0.2',
-      description='Module with tools to organize and work with tabular data (from CSV files or Pandas Data Frames).',
-      long_description=readme(),
-      url='https://github.com/Asta1986/utils_tabdata',
-      author='Asta1986',
-      author_email='psljp@protonmail.com',
-      license='GPL-3',
-      packages=find_packages(),
-      install_requires=['click==7.1.2', 'matplotlib==3.3.3', 'pandas==1.2.0', 'xlrd==1.2.0', 'xlsxwriter==1.3.7'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      entry_points='''
-                   [console_scripts]
-                   utils_tabdata=utils_tabdata.scripts:cli
-                   ''',
-      include_package_data=True,
-      zip_safe=False)
+    version='0.2',
+    description='Module with tools to organize and work with tabular data (from CSV files or Pandas Data Frames).',
+    long_description=readme(),
+    url='https://github.com/Asta1986/utils_tabdata',
+    author='Asta1986',
+    author_email='psljp@protonmail.com',
+    license='GPL-3',
+    packages=find_packages(),
+    python_requires='~=3.9',
+    install_requires=[
+        'click~=7.1',
+        'matplotlib~=3.3',
+        'pandas~=1.2',
+        'xlrd~=1.2',
+        'xlsxwriter~=1.3'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    entry_points='''
+                [console_scripts]
+                utils_tabdata=utils_tabdata.scripts:cli
+                ''',
+    include_package_data=True,
+    zip_safe=False
+)
